a011b196ed5f7b8f6c5748afb48733d91425d940
lly_cloud <lly_cloud@qq.com>
2019-08-19 02:34:00 +0800

    已经调试正确但是可能因为常数太大而Tle了

---
+++ Aug_16/Flower/test_atan2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cout<<atan2(1,0)<<endl;
+	cout<<atan2(1,1)<<endl;
+	cout<<atan2(0,1)<<endl;
+	cout<<atan2(-1,1)<<endl;
+	cout<<atan2(-1,0)<<endl;
+	cout<<atan2(-1,-1)<<endl;
+	cout<<atan2(0,-1)<<endl;
+	cout<<atan2(1,-1)<<endl;
+	cout<<atan2(1,0)<<endl;
+}
